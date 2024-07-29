29321cf60768ece7e86975556a4f45df025c6cbd
Ashu Singla <ashusingla884@gmail.com>
2024-07-29 17:52:41 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home/home';
+import React, { Suspense, lazy } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Header from './components/Header/header';
+import './App.scss';
+
+// Lazy load components
+const Home = lazy(() => import('./components/Home/home'));
+
+const routes = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+];
+
+const router = createBrowserRouter(
+  routes.map((route) => ({
+    ...route,
+    element: <Suspense fallback={<div>Loading...</div>}>{route.element}</Suspense>,
+  })),
+);
 
 function App() {
   return (
-    <div className='w-fit'>
+    <>
       <Header />
-      <Home />
-    </div>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
